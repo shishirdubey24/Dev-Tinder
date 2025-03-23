741273c73df4ae76bfbade997b8ff2e975efdfdd
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-23 17:44:32 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import NavBar from './components/NavBar'
+import Login from './components/Login'
+import Body from './components/Body'
+import Profile from './components/Profile'
+ 
 function App() {
  
   return (
   <>
-    <h1 className='font-extrabold '>jgbduiogberiu</h1>
-  </>
+    <BrowserRouter  basename='/'>
+     <Routes>
+
+     <Route path='/' element={<Body/>} >
+
+     <Route path='/login' element={<Login/>} />
+     <Route path='/Profile' element={<Profile/>} />
+      </Route>
+     </Routes>
+    </BrowserRouter>
+    
+      </>
   )
 }
 
