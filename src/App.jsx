c61b649405ff198d9bd5bc39939c3727e5b1623b
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-25 12:32:29 +0530

    Logic Flow Improvement

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import NavBar from './components/NavBar'
 import Login from './components/Login'
 import Body from './components/Body'
 import Profile from './components/Profile'
+import Browse from './components/Browse'
+import { Provider } from 'react-redux'
+import Store from './utils/Store'
  
 function App() {
  
   return (
   <>
+   <Provider store={Store}>  
     <BrowserRouter  basename='/'>
      <Routes>
 
@@ -17,10 +21,11 @@ function App() {
 
      <Route path='/login' element={<Login/>} />
      <Route path='/Profile' element={<Profile/>} />
+     <Route path='/Browse' element={<Browse/>}/>
       </Route>
      </Routes>
     </BrowserRouter>
-    
+    </Provider>
       </>
   )
 }
