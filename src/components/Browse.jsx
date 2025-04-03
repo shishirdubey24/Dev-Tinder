55376c4a34b80d234930cf51bbf1cf4f9a9d93f2
shishirdubey24 <shishirdubey221@gmail.com>
2025-04-03 14:45:35 +0530

    Added Login checking

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,18 @@
-
+import { useSelector } from "react-redux"
+//import Store from "../utils/Store";
 const Browse = () => {
+  
+   const user =useSelector((state) => state.user);
+   console.log("the Browser data is ",user);
   return (
     <div>
-      <h1>This is Browse Field</h1>
-    </div>
+    <h1>This is Browse Field</h1>
+    {user ? (
+      <p>Welcome, {user.email}!</p>
+    ) : (
+      <p>No user found. Please log in.</p>
+    )}
+  </div>
   )
 }
 
